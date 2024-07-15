fb174fc6f78f4b4d8dce19a0e8e494c3981abae7
Jhoandry Sequera <jsequera826@gmail.com>
2024-07-15 21:04:55 +0200

    EventList: Event Service which return list of events already stored on cache and a simple test

---
+++ src/main/java/com/prewave/eventTrace/services/implementation/EventServiceImpl.java
@@ -0,0 +1,37 @@
+package com.prewave.eventTrace.services.implementation;
+
+import com.prewave.eventTrace.repositories.models.Event;
+import com.prewave.eventTrace.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventServiceImpl implements EventService {
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Value("${prewave.event.cacheName}")
+    protected String eventCacheName;
+
+    @Override
+    public List<Event> fetchEvents() {
+        return getEvents();
+    }
+
+    private List<Event> getEvents() {
+        try {
+            Cache eventsCached = cacheManager.getCache(eventCacheName);
+            return eventsCached != null ? eventsCached.get("events", List.class) : List.of();
+        } catch (Exception e) {
+            System.err.println("Error getting Events, error message: " + e.getMessage());
+        }
+
+        return List.of();
+    }
+}
---
+++ src/test/java/com/prewave/eventTrace/services/implementation/EventServiceImplTest.java
@@ -0,0 +1,57 @@
+package com.prewave.eventTrace.services.implementation;
+
+import com.prewave.eventTrace.repositories.models.Event;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class EventServiceImplTest {
+
+    @Autowired
+    private EventServiceImpl eventService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Value("${prewave.event.cacheName}")
+    protected String eventCacheName;
+
+    @BeforeEach
+    void setUp() {
+        List<Event> events = Arrays.asList(
+                new Event(1, 1, 0, List.of()),
+                new Event(1, 1, 0, List.of()));
+
+        cacheEvents(events);
+    }
+
+
+    @Test
+    @DisplayName("Should return events from cache")
+    void shouldReturnEventsFromCache() {
+        // When
+        List<Event> events = eventService.fetchEvents();
+
+        // Then
+        assertEquals(2, events.size());
+        assertEquals(events, events);
+    }
+
+    private void cacheEvents(List<Event> events) {
+        Cache eventsCached = cacheManager.getCache(eventCacheName);
+        if (eventsCached != null) {
+            eventsCached.put("events", events);
+        }
+    }
+}
