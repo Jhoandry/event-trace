58f024238872886cc94e85578033e11ace03c08e
Jhoandry Sequera <jsequera826@gmail.com>
2024-07-16 22:55:22 +0200

    Final Checks: configure initialDelay for schedulers

--- src/main/java/com/prewave/eventTrace/services/process/EventScheduler.java
+++ src/main/java/com/prewave/eventTrace/services/process/EventScheduler.java
@@ -3,13 +3,15 @@ package com.prewave.eventTrace.services.process;
 import com.prewave.eventTrace.services.EventTraceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EventScheduler {
 
     @Autowired
     private EventTraceService eventTraceService;
 
-    @Scheduled(cron = "0 */5 * * * *")
+    @Scheduled(initialDelay=60000, fixedRate=120000)
     public void updateEvents() {
         eventTraceService.syncEvents();
     }
--- src/main/java/com/prewave/eventTrace/services/process/QueryTermScheduler.java
+++ src/main/java/com/prewave/eventTrace/services/process/QueryTermScheduler.java
@@ -3,19 +3,16 @@ package com.prewave.eventTrace.services.process;
 import com.prewave.eventTrace.services.TermsSyncService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+@Service
 public class QueryTermScheduler {
 
     @Autowired
     private TermsSyncService termsSyncService;
 
-    @Scheduled(fixedRate = 0)
+    @Scheduled(initialDelay=3000, fixedRate=900000)
     public void startupTask() {
-        termsSyncService.fetchQueryTerms();
-    }
-
-    @Scheduled(cron = "0 0 */1 * * *")
-    public void updateQueryTerms() {
         termsSyncService.fetchQueryTerms();
     }
 }
